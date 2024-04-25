cd1f44c18eb1b6d2ff3b1a9f94235eed892fbc9d
Captain Storm <CaptainStorm21@users.noreply.github.com>
2024-04-24 17:47:45 -0700

    16. JPA Relationships

--- src/main/java/guru/springframework/domain/Author.java
+++ src/main/java/guru/springframework/domain/Author.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+import java.util.Set;
 
 // if we use jakarta persistence, 
 // this is what is telling JPA that is going to be an entity persisted to the database
@@ -15,6 +18,20 @@ public class Author {
   private Long id;
   private String firstName;
   private String lastName;
+
+  // we are going to use annotation ManyToMany, meaning that we have have authors to many books
+  @ManyToMany(mappedBy = "authors")
+  private Set<Book> books;
+
+  public Set<Book> getBooks() {
+    return books;
+  }
+
+  public void setBooks(Set<Book> books) {
+    this.books = books;
+  }
+
+
 
 
   public Long getId() {
--- src/main/java/guru/springframework/domain/Book.java
+++ src/main/java/guru/springframework/domain/Book.java
@@ -1,10 +1,16 @@
 package guru.springframework.domain;
 
-import jakarta.annotation.Generated;
+import java.util.Set;
+
+import org.hibernate.annotations.ManyToAny;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+
 
 @Entity
 public class Book {
@@ -15,6 +21,20 @@ public class Book {
   private Long id;
   private String title;
   private String isbn;
+
+  @ManyToAny
+  @JoinTable (name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+    inverseJoinColumns = @JoinColumn(name = "author_id"))
+
+  private Set<Author> authors;
+
+  public Set<Author> getAuthors() {
+    return authors;
+  }
+
+  public void setAuthors(Set<Author> authors) {
+    this.authors = authors;
+  }
 
   public Long getId() {
     return this.id;
