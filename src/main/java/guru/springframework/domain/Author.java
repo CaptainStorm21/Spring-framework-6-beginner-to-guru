2868019a35278fa764efe59d95465230770f50fa
Captain Storm <CaptainStorm21@users.noreply.github.com>
2024-04-24 22:22:12 -0700

    18. Equality in Hibernate

--- src/main/java/guru/springframework/domain/Author.java
+++ src/main/java/guru/springframework/domain/Author.java
@@ -57,5 +57,52 @@ public class Author {
   public void setLastName(String lastName) {
     this.lastName = lastName;
   }
+
+  /*
+   * This code is implementing the equals() and hashCode() methods typically used
+   * for object comparison and hashing in Java.
+   * 
+   * The equals() method checks if two objects are equal based on their contents
+   * rather than their references. In this case, it checks if the objects are the
+   * same reference (this == o) or if the object being compared (o) is an instance
+   * of the Author class and has the same ID.
+   * 
+   * The hashCode() method returns a hash code value for the object, which is used
+   * by hash-based data structures like HashMap. In this case, it uses the ID of
+   * the Author object to generate the hash code.
+   * 
+   * Position your cursor inside the class where you want to generate the methods.
+   * Press Ctrl+Shift+P to open the command palette.
+   * Type "Generate equals and hashCode methods" and select the corresponding
+   * command
+   * 
+   */
+
+
+  @Override
+  public String toString() {
+    return "Author{" +
+            "id=" + id + 
+            ",  firstName = ' " + '\'' +
+            ",  lastName = ' " + '\'' +
+            ",  books = " + books +
+            '}';
+
+  }
+
   
+ @Override 
+  public boolean equals(Object o){
+    if (this == o ) return true;
+    if(!(o instanceof Author)) return false;
+
+    Author author = (Author) o;
+
+    return getId() !=null ? getId().equals(author.getId()) : author.getId() == null;
+   }
+
+   @Override
+   public int hashCode(){
+    return getId() != null ? getId().hashCode() : 0;
+   }
 }
--- src/main/java/guru/springframework/domain/Book.java
+++ src/main/java/guru/springframework/domain/Book.java
@@ -10,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
+import java.util.Objects;
 
 
 @Entity
@@ -59,5 +60,44 @@ public class Book {
   public void setIsbn(String isbn) {
     this.isbn = isbn;
   }
+  
 
+
+  /*
+   * This code is implementing the equals() and hashCode() methods typically used
+   * for object comparison and hashing in Java.
+   * 
+   * The equals() method checks if two objects are equal based on their contents
+   * rather than their references. In this case, it checks if the objects are the
+   * same reference (this == o) or if the object being compared (o) is an instance
+   * of the Author class and has the same ID.
+   * 
+   * The hashCode() method returns a hash code value for the object, which is used
+   * by hash-based data structures like HashMap. In this case, it uses the ID of
+   * the Author object to generate the hash code.
+   * 
+   * Position your cursor inside the class where you want to generate the methods.
+   * Press Ctrl+Shift+P to open the command palette.
+   * Type "Generate equals and hashCode methods" and select the corresponding
+   * command
+   * 
+   */
+ 
+
+  @Override
+  public boolean equals(Object o){
+    if (this == o ) return true;
+    if(!(o instanceof Author)) return false;
+
+    Author author = (Author) o;
+
+    return getId() !=null ? getId().equals(author.getId()) : author.getId() == null;
+   }
+
+   @Override
+   public int hashCode(){
+    return getId() != null ? getId().hashCode() : 0;
+   }
 }
+
+
